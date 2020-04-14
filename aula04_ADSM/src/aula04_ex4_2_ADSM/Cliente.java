13dfd5484f2ec82aefe7006bfe543799776f2c0c
GuiPardini <61882893+GuiPardini@users.noreply.github.com>
2020-04-13 21:54:37 -0300

    Update Cliente.java

--- aula04_ADSM/src/aula04_ex4_2_ADSM/Cliente.java
+++ aula04_ADSM/src/aula04_ex4_2_ADSM/Cliente.java
@@ -8,6 +8,7 @@ public class Cliente {
 	}
 
 	public void executar() {
+		//vetor de inteiros
 		int[] vetor = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
 		int soma =somador.somaVetor(vetor); 
 		System.out.println("Resultado: " +soma); 
